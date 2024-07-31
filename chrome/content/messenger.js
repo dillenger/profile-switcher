15f9e7ef81b03497904d544164c4db145e836137
dillenger <dillinger@kpnmail.nl>
2024-07-31 07:25:17 +0200

    update

---
+++ chrome/content/messenger.js
@@ -0,0 +1,8 @@
+
+function onLoad(activatedWhileWindowOpen) {
+  // We do not load anything anymore, but we need at least one registered
+  // overlay to get the options page registered.
+}
+
+function onUnload(deactivatedWhileWindowOpen) {
+}
